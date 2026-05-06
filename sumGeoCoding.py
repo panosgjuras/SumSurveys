b782c1aa4c6b266890802863c2e4b4cc6f595d18
panosgjuras <63541107+panosgjuras@users.noreply.github.com>
2026-05-06 19:55:36 +0300

    modal splits vs Distance, first results

--- sumGeoCoding.py
+++ sumGeoCoding.py
@@ -13,72 +13,49 @@ import os
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 
-
 # TODO: fix the pathing issue
 root_dir = "/Users/panosgtzouras/Desktop/datasets/csv/SUMsurveyData/finalDatasets"
-df = pd.read_csv(os.path.join(root_dir, "SumSurveyDiariesV2.0.2.csv"))
 
-df['orig2'] = df['orig'].astype(str) + ", " + df['city']
-df['dest2'] = df['dest'].astype(str) + ", " + df['city']
+# %% Main functions
 
-# unique locations
-locations = pd.concat([df['orig2'], df['dest2']]).dropna().drop_duplicates()
-locations = pd.DataFrame({'location': locations})
+def sum_geocoding(df):
+    
+    df2 = df.copy()
+    
+    df2['orig2'] = df2['orig'].astype(str) + ", " + df['city']
+    df2['dest2'] = df2['dest'].astype(str) + ", " + df['city']
+    
+    locations = pd.concat([df2['orig2'], df2['dest2']]).dropna().drop_duplicates()
+    locations = pd.DataFrame({'location': locations})
 
+    geolocator = Nominatim(user_agent="sum_living_labs_geocoder")
+    
+    geocode = RateLimiter(
+        geolocator.geocode,
+        min_delay_seconds=1,
+        max_retries=3
+    )
+    
+    locations['location'] = locations['location'].dropna()
+    
+    locations['geo'] = locations['location'].apply(geocode)
+    
+    locations['lat'] = locations['geo'].apply(lambda x: x.latitude if x else None)
+    locations['lon'] = locations['geo'].apply(lambda x: x.longitude if x else None)
+    
+    return locations
 
-# geocoder
-geolocator = Nominatim(user_agent="sum_living_labs_geocoder")
-
-geocode = RateLimiter(
-    geolocator.geocode,
-    min_delay_seconds=1,
-    max_retries=3
-)
-
-# add city/country context if useful
-
-locations['location'] = locations['location'].dropna()
-
-locations['geo'] = locations['location'].apply(geocode)
-
-locations['lat'] = locations['geo'].apply(lambda x: x.latitude if x else None)
-locations['lon'] = locations['geo'].apply(lambda x: x.longitude if x else None)
-
-print(locations.head(10))
-
-# %% Save locations
-
-# TODO: make it a function with version and location as inputs to call it again and again
-version = "_v1"
-
-# Keep only valid coordinates
-gdf = locations.dropna(subset=['lat', 'lon']).copy()
-gdf = gdf.drop(columns=['geo'], errors='ignore').copy()
-
-# Create geometry column
-gdf['geometry'] = gdf.apply(
-    lambda row: Point(row['lon'], row['lat']),
-    axis=1
-)
-
-# Convert to GeoDataFrame (WGS84)
-gdf = gpd.GeoDataFrame(gdf, geometry='geometry', crs="EPSG:4326")
-
-# Save to GeoPackage
-gdf.to_file(
-    os.path.join(root_dir, f"geocoded_locations{version}.gpkg"),
-    layer="locations",
-    driver="GPKG"
-)
-
-# %% Manual fixing
-
-# Rotterdam has no origin-destination points
-
-# TODO: Import the gpkg with location, do not run again geolocation
-# TODO: find the coordinates of the failed points using Google Maps
-# TODO: inspect the map and clean some wrong geolocated points
-# TODO: save a new version and inspect again on GIS
+def make_gpkg(locations, version):
+    gdf = locations.dropna(subset=['lat', 'lon']).copy()
+    gdf = gdf.drop(columns=['geo'], errors='ignore').copy()
+    gdf['geometry'] = gdf.apply(
+        lambda row: Point(row['lon'], row['lat']),
+        axis=1)
+    gdf = gpd.GeoDataFrame(gdf, geometry='geometry', crs="EPSG:4326")
+    gdf.to_file(
+        os.path.join(root_dir, f"geocoded_locations{version}.gpkg"),
+        layer="locations",
+        driver="GPKG")
 
 def failed_geocode(df):
     # Failed geocoding cases
@@ -91,16 +68,6 @@ def failed_geocode(df):
     print(x)
     return x
 
-failed = failed_geocode(locations)
-
-manual_coords = {
-    '610: Σπάτα, Ν. Μάκρη, Ραφήνα, Μαραθώνας, Athens': (37.968191, 23.910175),
-    '105: Σισμανόγλειο, Καμάρες, Athens': (37.968191, 23.910175),
-    
-}
-
-print(manual_coords)
-
 def fill_manual_coords(row):
     if pd.isna(row['lat']) or pd.isna(row['lon']):
         coords = manual_coords.get(row['location'])
@@ -108,9 +75,114 @@ def fill_manual_coords(row):
             return pd.Series([coords[0], coords[1]])
     return pd.Series([row['lat'], row['lon']])
 
+def manual_coords_update(locations, updates):
+    for loc, coords in updates.items():
+        locations.loc[
+            locations['location'] == loc,
+            ['lat', 'lon']
+        ] = coords
+    return locations
+
+# %% Automatic geolocation
+
+df = pd.read_csv(os.path.join(root_dir, "SumSurveyDiariesV2.0.2.csv"))
+
+locations = sum_geocoding(df) # DO NOT RUN IT AGAIN
+
+version = "_v1"
+make_gpkg(locations, version)
+
+# %% Manual fixing of geolocations
+
+# Rotterdam has no origin-destination points
+# Larnaca has no origin-destination points
+# Fredrikstad are completely missing, no survey
+
+# find the coordinates of the failed points using Google Maps
+failed = failed_geocode(locations)
+
+manual_coords = {
+ '610: Σπάτα, Ν. Μάκρη, Ραφήνα, Μαραθώνας, Athens' :(37.968191, 23.910175),
+ '105: Σισμανόγλειο, Καμάρες, Athens' '100: Πεντέλη Βουνό, Athens' :(37.968191, 23.910175),
+ '103: Βίγλα, Τερψιθέα, Athens': (38.06176776739204, 23.840848693994758),
+ '106: Αμαλία Φλέμινγκ, 25ης Μαρτίου, Athens' : (38.054684149837534, 23.834869872239793),
+ '107: Νεόκτιστα, Παλιαγιάννης, Σαλίγκαρος, Κακιά Σκάλα, Athens': (37.9838096, 23.7275388),
+ '104: Αγ. Μαρίνα, Μαγερίνα, Athens': (38.05398172, 23.84568085),
+ '605: Άλιμος, Γλυφάδα, Ηλιούπολη, Αργυρούπολη, Ελληνικό, Athens': (37.9168548, 23.7186146),
+ '500: Αθήνα κέντρο, Athens': (37.9838096, 23.7275388),
+ '609: Κορωπί, Μαρκόπουλο, Λαύριο, Ανάβυσσος, Athens': (37.8833564, 23.9333037),
+ '606: Περιστέρι, Ίλιον, Αγ. Ανάργυροι, Athens': (0,0),
+ '607: Ν. Ηράκλειο, Ν. Ιωνία, Ν. Φιλαδέλφεια, Γαλάτσι, Athens': (38.04065578623009, 23.768933209296243),
+ '611: Αχαρνές, Αγ. Στέφανος, Διόνυσος, Ωρωπός, Athens': (37.9838096, 23.7275388),
+ '602: Χαϊδάρι, Αιγάλεω, Κορυδαλλός, Νίκαια, Athens': (37.9845573, 23.6478153),
+ '612: Φυλή, Ασπρόπυργος, Ελευσίνα, Μέγαρα, Athens': (37.9952107, 23.345307),
+ '603: Καλλιθέα, Ταύρος, Μοσχάτο, Athens' : (37.95615269101097, 23.702723402732506),
+ 'Yuvalim Ganim, Jerusalem' : (31.753710248868394, 35.17049803998727),
+ 'Givat Masua, Jerusalem' : (31.750123194366456, 35.16821437727791),
+ 'Reches Lavan, Jerusalem' : (31.748979459517116, 35.15957622798716),
+ 'Bayit VeGan Ramat Sharet, Jerusalem' : (31.769680280199672, 35.184775797287294),
+ "Ma'ar (CBD / City Center), Jerusalem" : (31.7810, 35.2196),
+ 'Rasco, Jerusalem' : (31.762678266641068, 35.20303049201401),
+ 'Givat Shaul Industrial Zone, Jerusalem': (31.792136635438947, 35.19488728824738),
+ 'Baka Moshavot, Jerusalem': (31.759258496743644, 35.2196601096048),
+ "Nachlaot Sha'arei Chesed, Jerusalem": (31.78623005374961, 35.20878695030782),
+ 'Makor Baruch, Jerusalem': (31.789182244889705, 35.2119083298815),
+ 'Talpiot Industrial Zone, Jerusalem': (31.752380094402277, 35.21327810380562),
+ 'Gush Etzion, Jerusalem': (31.6690, 35.1215),
+ 'veyrier lancy bachet a pied, train a Berne retour, Geneva': (46.16605286752029, 6.178382570905729),
+ 'Bachet vers Rte de Veyrier, Geneva' : (46.17629292911239, 6.12393094313005),
+# 'Pour aller et rentrer du travail, Geneva' : (0,0),
+# "Départ de chez moi à vélo. 800m de trajet. Arrivée au bureau. Départ du bureau 900m de trajet. Arrivée au rdv. Départ du rdv jusqu'au magasin de vélo, 300m. Retour à pied chez moi, Geneva" : (0,0),
+ 'Genève centre à Genève centre (achats alimentaires), Geneva' : (46.20491348355323, 6.142859388818459),
+# "je suis en déplacement à l'étranger. Dans la même commune j'ai dû marcher environ 45 min., Geneva" : (0,0),
+# 'maison -> travail, travail -> maison, Geneva' : (0,0),
+ 'Je me suis rendue de mon logement à mon travail à vélo depuis Plan-les-Ouates., Geneva' : (46.166827077699566, 6.1136861414363475),
+ 'Leysin, Geneva' 'domicile à Nyon, Geneva' : (46.34576932688499, 7.013660918113015),
+# 'Aproz (Nendaz/VS), Geneva' ,
+ 'Collonges Bellevue, Geneva' : (46.141695322415075, 6.144781879843948),
+ 'Vernie, Geneva' : (46.211506492011964, 6.1073468902625825),
+# 'retour Bern Veyrier, Geneva' : (0,0),
+# 'De mon domicile au centre commercial , Geneva' : (0,0),
+ 'Bois Ecard vers Place des Aviateurs, Geneva' : (46.16859694350549, 6.120347942356223),
+# "Depuis mon travail jusqu'à un rdv. 900m à vélo, Geneva" : (0,0),
+ 'genève- arrêt acacias, Geneva' : (46.19282553719367, 6.13839261854068),
+ 'servette (travail UOG), Geneva' : (46.213647191668244, 6.129871208011474),
+ 'renens, Geneva' : (46.53378659954073, 6.591410304694897),
+ 'Gland, Geneva' : (46.42046614375563, 6.267261604121123), 
+ 'Wiedlisbach, Geneva' : (47.25118334826089, 7.645360572926388),
+# 'La Givrine, Geneva'
+ 'St-Sulpice (VD), Geneva' : (46.51005327498263, 6.557789601499271),
+# "Retour à pied car j'ai laissé mon vélo chez le réparateur, Geneva" : (0,0),
+ 'Portagem | Polo I (Universidade), Coimbra' : (40.20760775249469, -8.430180925341096),
+ 'S. Martinho | Santa Clara | Covões | Iparque, Coimbra' : (40.19421130396164, -8.451313025820557),
+ 'Umm al-Fahm, Jerusalem' : (32.517093772674436, 35.14831060354167),
+ "Haredi Ma'ar, Jerusalem" : (31.7857, 35.2137),
+ 'Zichron Yaacov, Jerusalem':(31.79504094202089, 35.20123095344182),
+ 'Lutry, Geneva' : (46.50239079325718, 6.685561141311649),
+# "J'ai été faire des courser et découvrir les rues environnantes de mon leu de résidence, Geneva" : (0,0),
+ 'genève -arrêt tram acacias, Geneva' : (46.19285522066536, 6.138371179820004),
+ 'Augustins (HUG), Geneva' : (46.19204938914605, 6.14378671365993),
+# 'Le Brassus, col des Amburnex, Geneva' :(),
+ 'Nyon cebtre ville, Geneva' : (46.3833, 6.2390),
+ 'Ferney-Voltaire, Geneva' : (46.25800967312653, 6.109694532954785),
+# 'emyrin, Geneva' : (0,0),
+ 'Nyon Migros Porte de Nyon, Geneva' : (46.38833654782171, 6.220671649136077)}
+ 
 locations[['lat', 'lon']] = locations.apply(
     fill_manual_coords,
     axis=1
 )
+# version = "_v2"
+# make_gpkg(locations, version)
 
+# inspect the map and clean some wrong geolocated points
+drop_locations = ["nan, Krakow", "nan, Jerusalem", "nan, Rotterdam"]
+locations = locations[ ~locations['location'].isin(drop_locations)].copy()
 
+# Give the locations, for which coordiations have to be updated
+updates = {"The Old City, Jerusalem": (31.7767, 35.2345)} 
+locations = manual_coords_update(locations, updates)
+
+# save a new version and inspect again on GIS
+version = "_v3"
+make_gpkg(locations, version)
--- sumModalShifts.py
+++ sumModalShifts.py
@@ -13,11 +13,27 @@ import os
 os.chdir("/Users/panosgtzouras/Desktop/github_tzouras/SumSurveys/SumSurveysTools")
 from sumSurveyReplacer import genRandomTime, rePlacer
 from sumAssessAnalysis import plotModalSplit3
-# import geopandas as gpd
-
+import geopandas as gpd
+from scipy.spatial import distance_matrix
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 # TODO: fix the pathing, download the data from Zenodo
 root_dir = "/Users/panosgtzouras/Desktop/datasets/csv/SUMsurveyData/finalDatasets"
-
+color_map = {
+    'car': '#004494',
+    'taxi': '#FCF008',
+    'train': '#dd8452',
+    'bus': '#FF632F',
+    'motorcycle': '#da8bc3',
+    'bicycle': '#98C33A',
+    'escooter': '#75BDFB',
+    'walk': '#DADADA',
+    'car sharing': '#2D8CFF',
+    'micromobility': '#C4DD8B',
+    'ride hailing': '#FFC2AF',
+    'ferry': 'grey'
+}
 # %% Import the before dataset
 
 # TODO: fix the pathing, download the data from Zenodo
@@ -26,6 +42,17 @@ root_dir = "/Users/panosgtzouras/Desktop/datasets/csv/SUMsurveyData/finalDataset
 df1 = pd.read_csv(os.path.join(root_dir, "SumSurveyDiariesV2.csv"))
 print(df1.columns)
 
+df1 = df1.drop(columns = ['orig_latitude', 'orig_longitude', 'dest_latitude', 'dest_longitude', 'distance'])
+df1 = df1.rename(columns = {'fdistance':'distance'})
+df1['distance'] = df1['distance'].replace(5, 1)
+
+version = "_v2.0.1"
+df1.copy().to_csv(
+    os.path.join(root_dir, f"SumSurveyDiaries{version}.csv"),
+    index=False
+) # this dataset is the same with V1 but it is the ex-post
+
+
 socio1 = pd.read_csv(os.path.join(root_dir, "SumSurveySocioV1.csv"))
 # print(socio1.columns)
 
@@ -33,7 +60,6 @@ socio1 = pd.read_csv(os.path.join(root_dir, "SumSurveySocioV1.csv"))
 
 # TODO: remove this section, it is not necessary
 init_csv = "/Users/panosgtzouras/Library/CloudStorage/OneDrive-UniversityofWestAttica/TZOURAS_paperz/paper49_sumImpact/ex_ante_dataset/SumSurveyDiaries_FINAL_with_Demographics.csv"
-
 df = pd.read_csv(init_csv)
 # print(df.columns)
 # print(df.survey_type.unique())
@@ -50,7 +76,7 @@ df_expost = df[df["survey_type"] == "ExPost"].copy() # keep only the expost to c
 # _v1.0.2 ex-post no distance
 # _v2.0.1 ex-ante with distances, it is the same with V2
 # _v2.0.2 ex-post with distances
-version = "_v1.0.2"
+
 
 df2 = df_expost[
     ['mode', 'purp', 'time', 'orig', 'dest', 'pid', 'city',
@@ -63,6 +89,13 @@ df2['time'] =  df2['time'].apply(genRandomTime)
 df2 = rePlacer(df2, 'mode')
 df2 = rePlacer(df2, 'purp')
 
+# version = "_v2.0.2"
+# df2.copy().to_csv(
+#     os.path.join(root_dir, f"SumSurveyDiaries{version}.csv"),
+#     index=False
+# ) # this dataset is the same with V1 but it is the ex-post
+
+version = "_v1.0.2"
 df2[['mode', 'purp', 'time', 'pid', 'city']].copy().to_csv(
     os.path.join(root_dir, f"SumSurveyDiaries{version}.csv"),
     index=False
@@ -70,16 +103,186 @@ df2[['mode', 'purp', 'time', 'pid', 'city']].copy().to_csv(
 
 # %% Classic pies
 
-df2 = pd.read_csv(os.path.join(root_dir, "SumSurveyDiaries_v1.0.2.csv"))
 cIE = df2.city.unique() # Fredrikstad is missing from the data
 for c in cIE: plotModalSplit3(df2, c) # Develop the classical modal split pies
 
 # %% Add distance
 
-df2.to_csv(
-    os.path.join(root_dir, "SumSurveyDiariesV2.0.2.csv"),
-    index=False
+centroids = gpd.read_file(
+    os.path.join(root_dir, "geocoded_locations_v3.gpkg"),
+    layer="locations"
 )
+
+df2 = pd.read_csv(os.path.join(root_dir, "SumSurveyDiaries_v2.0.2.csv"))
+
+def createDistDf2(centroids):
+    # make sure CRS is WGS84 first
+    centroids = centroids.set_crs("EPSG:4326", allow_override=True)
+    # project to meters
+    centroids_m = centroids.to_crs("EPSG:3857")
+    coords = centroids_m.geometry.apply(lambda geom: (geom.x, geom.y)).tolist()
+    dist_matrix = distance_matrix(coords, coords)
+    # convert meters to km and apply detour factor
+    dist_matrix = (1.3 * dist_matrix) / 1000
+    zone_codes = centroids_m['location']
+    dist_df = pd.DataFrame(
+        dist_matrix,
+        index=zone_codes,
+        columns=zone_codes
+    )
+    return dist_df
+
+distDf = createDistDf2(centroids)
+distDf[distDf < 0.001] = 1 # This is the case of intra-zonal trips, they are assumed equal to 5 km
+
+def get_distance(orig_zone, dest_zone):
+    try:
+        return distDf.at[orig_zone, dest_zone]
+    except KeyError:
+        return None
+
+def findTripDistances2(df):
+    df['orig2'] = df['orig'].astype(str) + ", " + df['city']
+    df['dest2'] = df['dest'].astype(str) + ", " + df['city']
+    df['distance'] = df.apply(
+        lambda row: get_distance(row['orig2'], row['dest2']),
+        axis=1
+    )
+    return df
+
+df2 = findTripDistances2(df2)
+# print(df2.columns)
+df2 = df2.drop(columns=['orig_latitude','orig_longitude', 'dest_latitude', 'dest_longitude', 
+               'fdistance', 'orig2', 'dest2'])
+print(len(df2))
+# Keep only rows with valid finite distances
+df2 = df2[df2['distance'].notna() & np.isfinite(df2['distance'])].copy()
+print(len(df2))
+
+version = "_v2.0.2"
+df2.copy().to_csv(
+    os.path.join(root_dir, f"SumSurveyDiaries{version}.csv"),
+    index=False
+) # this dataset is the same with V1 but it is the ex-post
+
+# %%
+
+# print(df1.columns)
+# print(df2.columns)
+
+exclude_cities = ['Fredrikstad', 'Larnaca', 'Rotterdam']
+df1 = df1[~df1['city'].isin(exclude_cities)].copy()
+
+# Distance bins: 0-5, 5-10, ..., 35-40, 40+
+# bins = [0, 5, 10, 15, 20, 25, np.inf]
+# labels = ['0-5', '5-10', '10-15', '15-20', '20-25',
+#           '25+']
+
+bins = [0, 2.5, 5, 7.5, 10, 12.5, 15, 17.5, 20, 22.5, 25, np.inf]
+
+labels = [
+    '0-2.5',
+    '2.5-5',
+    '5-7.5',
+    '7.5-10',
+    '10-12.5',
+    '12.5-15',
+    '15-17.5',
+    '17.5-20',
+    '20-22.5',
+    '22.5-25',
+    '25+'
+]
+
+def modeShareDistStats(df):
+    temp = df.copy()
+
+    temp['distance_bin'] = pd.cut(
+        temp['distance'],
+        bins=bins,
+        labels=labels,
+        right=False,
+        include_lowest=True)
+
+    counts = (
+        temp
+        .groupby(['distance_bin', 'mode'])
+        .size()
+        .reset_index(name='n'))
+
+    counts['share'] = counts.groupby('distance_bin')['n'].transform(
+        lambda x: 100 * x / x.sum()
+    )
+
+    pivot = counts.pivot(
+        index='distance_bin',
+        columns='mode',
+        values='share'
+    ).fillna(0)
+
+    # keep distance bin order
+    pivot = pivot.reindex(labels)
+
+    return pivot
+
+def is_dark(hex_color):
+    r, g, b = mcolors.to_rgb(hex_color)
+    luminance = 0.2126*r + 0.7152*g + 0.0722*b
+    return luminance < 0.5
+
+def stacked_ShareDist(df, title):
+    pivot = modeShareDistStats(df)
+
+    modes = [m for m in color_map.keys() if m in pivot.columns]
+    pivot = pivot[modes]
+    
+    fig, ax = plt.subplots(figsize=(8, 8), dpi=300)
+    
+    pivot.plot(
+        kind='bar',
+        stacked=True,
+        color=[color_map[m] for m in modes],
+        width = 0.8,
+        alpha = 0.8,
+        edgecolor='black',
+        ax=ax)
+    
+    ax.set_title(title)
+    ax.set_xlabel('Distance band (km)')
+    ax.set_ylabel('Mode share (%)')
+    ax.set_ylim(0, 100)
+    ax.legend(title='Transport mode', bbox_to_anchor=(1.02, 1), loc='upper left')
+
+    # Annotate percentages
+    for i, mode in enumerate(modes):
+    
+        color = color_map[mode]
+        text_color = 'white' if is_dark(color) else 'black'
+    
+        for container in [ax.containers[i]]:
+    
+            labels_txt = [
+                f'{v.get_height():.0f}%'
+                if v.get_height() >= 5 else ''
+                for v in container
+            ]
+    
+            ax.bar_label(
+                container,
+                labels=labels_txt,
+                label_type='center',
+                fontsize=10,
+                color=text_color,
+#                fontweight = 'bold'
+            )
+            
+    plt.tight_layout()
+    plt.show()
+
+
+remark = 'Rotterdam, Larnca, and Fredrikstad exluded'
+stacked_ShareDist(df1, f"Ex-ante mode share ({remark})")
+stacked_ShareDist(df2, f"Ex-post mode share ({remark})")
 
 
 # %% Create the socio dataframe
